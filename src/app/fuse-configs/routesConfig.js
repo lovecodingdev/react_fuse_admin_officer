22fb911367a9a1db4bb84a2a61bcae163e699c84
lovecoding <lovecodingdev@gmail.com>
2021-01-28 19:36:08 +1000

    no message

--- src/app/fuse-configs/routesConfig.js
+++ src/app/fuse-configs/routesConfig.js
@@ -32,7 +32,7 @@ const routes = [
 	{
 		path: '/',
 		exact: true,
-		component: () => <Redirect to="/apps/setup/bonus-plan/all" />
+		component: () => <Redirect to="/apps/dashboard/dashboard" />
 	},
 	{
 		component: () => <Redirect to="/pages/errors/error-404" />
